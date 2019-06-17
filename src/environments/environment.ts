01575ddb82e10add3438218c85c7295c43923ec8
geezlouisee <louisehl.lee@gmail.com>
2019-06-17 18:42:49 +0800

    Attempt to link to Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDZulF03VKogjQrZ7UTs95WlO3VlCD-ayQ',
+    authDomain: 'teambuilder-57269.firebaseapp.com',
+    databaseURL: 'https://teambuilder-57269.firebaseio.com',
+    projectId: 'teambuilder-57269',
+    storageBucket: 'teambuilder-57269.appspot.com',
+    messagingSenderId: '825456925398',
+    appId: '1:825456925398:web:97e2a43d919b8723'
+  }
 };
 
 /*
