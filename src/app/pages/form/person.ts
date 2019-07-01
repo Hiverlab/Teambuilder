65101caac6fe18af0327834bb85acddf8fbb1f06
geezlouisee <louisehl.lee@gmail.com>
2019-07-01 15:55:12 +0800

    added Person class

---
+++ src/app/pages/form/person.ts
@@ -0,0 +1,19 @@
+export class Person {
+  //field 
+  name: string;
+  executing: string;
+  influencing: string;
+  relationship: string;
+  strategic: string;
+
+  //constructor 
+  constructor(name: string, executing: string, influencing: string, relationship: string, strategic: string) {
+    this.name = name
+    this.executing = executing;
+    this.influencing = influencing;
+    this.relationship = relationship;
+    this.strategic = strategic;
+  }  
+
+
+}
