f447f8f1115fa748045ca2051ca9ef208dd02851
ditan96 <daryltan96@yahoo.com.sg>
2019-07-18 16:10:32 +0800

    Add skills score to overall score

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -41,7 +41,7 @@ export class FormComponent implements OnInit, AfterContentInit {
     this.personData = this.dataService.getPersons()
       .subscribe(
         (data: any) => {
-          console.log('Success!', data);
+          console.log('Success! Json data loaded.', data);
           this.createPersons(data['persons']);
         });
   }
@@ -64,16 +64,23 @@ export class FormComponent implements OnInit, AfterContentInit {
       let person = new Person();
       Object.assign(person, data[i]);
       this.personArray.push(person);
-      console.log(person.name);
+      console.log(person);
     }
     console.log(this.personArray);
   }
 
   goToPage(pagename:string, parameter:string) {
+    let skillsSelected: string[] = [];
+    for (let skill of this.skills) {
+      if (this.formModel[skill["field"]]) {
+        skillsSelected.push(skill["field"]);
+      }
+    }
+    console.log("Skills selected: " + skillsSelected);
     var sliderValues = this.getSliderValues();
     let scorePersonList: ScorePerson[][] = [];
     for (let category of this.formModel.category) {
-      scorePersonList.push(this.createScorePersons(category, sliderValues));
+      scorePersonList.push(this.createScorePersons(category, sliderValues, skillsSelected));
     }
     console.log(scorePersonList);
     this.router.navigate([pagename]);
@@ -86,14 +93,25 @@ export class FormComponent implements OnInit, AfterContentInit {
     return [strengthsPercentage, skillsPercentage];
   }
 
-  createScorePersons(key: string, sliderValues: number[]) {
-    this.personArray.sort((p1, p2) => (p1[key] < p2[key]) ? 1 : -1);
+  getSkillsScore(person: Person, skillsSelected: string[]) {
+    var skillsScore = 0;
+    for (let skill of skillsSelected) {
+      skillsScore += person[skill];
+    }
+    return skillsScore;
+  }
+
+  createScorePersons(key: string, sliderValues: number[], skillsSelected: string[]) {
     let scorePersonArray: ScorePerson[] = [];
     for (let person of this.personArray) {
       var gallupScore = person[key] * sliderValues[0] / 100;
-      var scorePerson = new ScorePerson(person, gallupScore);
+      // TODO: Convert skill score such that value is max y%
+      var skillsScore = this.getSkillsScore(person, skillsSelected) * sliderValues[1] / 100 / Math.max(1, skillsSelected.length);
+      console.log(person.name, "Gallup score: ", gallupScore, "Skills score: ", skillsScore);
+      var scorePerson = new ScorePerson(person, gallupScore + skillsScore);
       scorePersonArray.push(scorePerson);
     }
+    scorePersonArray.sort((p1, p2) => (p1.score < p2.score) ? 1 : -1);
     console.log(key, scorePersonArray);
     return scorePersonArray;
   }
